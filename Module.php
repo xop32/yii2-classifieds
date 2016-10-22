eda3755f87d3007d1db1ff780336f76d720baeb6
mohammad adibi <adibimohammad1365@gmail.com>
2016-10-22 21:55:06 +0330

    edit project

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace classifieds;
+
+use Yii;
+use yii\base\Module as BaseModule;
+
+/**
+ * This is just an example.
+ */
+class Module extends BaseModule
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
